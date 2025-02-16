eb610a06c386538ffa20c1cf9360c758e237b959
derek2403 <derekliew0@gmail.com>
2025-02-16 17:06:50 +0800

    house finish design

--- components/Environment.js
+++ components/Environment.js
@@ -1,25 +1,20 @@
 import * as THREE from 'three'
 import { useTexture } from '@react-three/drei'
+import { House } from './House'
 
 export function Environment() {
-  // Load textures
+  // Load grass texture
   const grassTexture = useTexture('/textures/grass.jpg')
-  const wallTexture = useTexture('/textures/wall.jpg')
-  const floorTexture = useTexture('/textures/floor.jpg')
 
-  // Configure texture repeating
+  // Configure grass texture repeating
   grassTexture.wrapS = grassTexture.wrapT = THREE.RepeatWrapping
-  grassTexture.repeat.set(10, 10)
-  wallTexture.wrapS = wallTexture.wrapT = THREE.RepeatWrapping
-  wallTexture.repeat.set(2, 2)
-  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping
-  floorTexture.repeat.set(4, 4)
+  grassTexture.repeat.set(20, 20)  // Increased repeat for larger area
 
   return (
     <group>
       {/* Grass ground */}
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
-        <planeGeometry args={[100, 100]} />
+        <planeGeometry args={[200, 200]} /> {/* Increased size for larger ground */}
         <meshStandardMaterial 
           map={grassTexture}
           roughness={1}
@@ -27,48 +22,8 @@ export function Environment() {
         />
       </mesh>
 
-      {/* Room */}
-      <group position={[0, 1.5, -5]}>
-        {/* Floor */}
-        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1.5, 0]} receiveShadow>
-          <boxGeometry args={[10, 10, 0.1]} />
-          <meshStandardMaterial 
-            map={floorTexture}
-            roughness={0.8}
-            metalness={0.2}
-          />
-        </mesh>
-
-        {/* Back Wall */}
-        <mesh position={[0, 0, -5]} receiveShadow castShadow>
-          <boxGeometry args={[10, 3, 0.1]} />
-          <meshStandardMaterial 
-            map={wallTexture}
-            roughness={0.8}
-            metalness={0.2}
-          />
-        </mesh>
-
-        {/* Left Wall */}
-        <mesh position={[-5, 0, 0]} rotation={[0, Math.PI / 2, 0]} receiveShadow castShadow>
-          <boxGeometry args={[10, 3, 0.1]} />
-          <meshStandardMaterial 
-            map={wallTexture}
-            roughness={0.8}
-            metalness={0.2}
-          />
-        </mesh>
-
-        {/* Right Wall */}
-        <mesh position={[5, 0, 0]} rotation={[0, -Math.PI / 2, 0]} receiveShadow castShadow>
-          <boxGeometry args={[10, 3, 0.1]} />
-          <meshStandardMaterial 
-            map={wallTexture}
-            roughness={0.8}
-            metalness={0.2}
-          />
-        </mesh>
-      </group>
+      {/* Add the house */}
+      <House />
     </group>
   )
 } 
---
+++ components/House.js
@@ -0,0 +1,62 @@
+import * as THREE from 'three'
+import { useTexture } from '@react-three/drei'
+
+export function House() {
+  // Load textures
+  const wallTexture = useTexture('/textures/wall.jpg')
+  const floorTexture = useTexture('/textures/floor.jpg')
+
+  // Configure texture repeating
+  wallTexture.wrapS = wallTexture.wrapT = THREE.RepeatWrapping
+  wallTexture.repeat.set(2, 2)
+  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping
+  floorTexture.repeat.set(8, 8)
+
+  const UNIT = 3 // Base unit
+  const WALL_HEIGHT = 3
+  const WALL_THICKNESS = 0.2
+
+  return (
+    <group position={[0, 0, 0]}>
+      {/* Main Floor */}
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
+        <boxGeometry args={[UNIT * 12, UNIT * 12, 0.2]} />
+        <meshStandardMaterial map={floorTexture} roughness={0.8} metalness={0.2} />
+      </mesh>
+
+      {/* Outer Walls */}
+      <Wall size={[UNIT * 12, WALL_HEIGHT, WALL_THICKNESS]} position={[0, WALL_HEIGHT/2, UNIT * 6]} texture={wallTexture} />
+      <Wall size={[UNIT * 12, WALL_HEIGHT, WALL_THICKNESS]} position={[0, WALL_HEIGHT/2, -UNIT * 6]} texture={wallTexture} />
+      <Wall size={[UNIT * 12, WALL_HEIGHT, WALL_THICKNESS]} position={[-UNIT * 6, WALL_HEIGHT/2, 0]} rotation={[0, Math.PI/2, 0]} texture={wallTexture} />
+      <Wall size={[UNIT * 12, WALL_HEIGHT, WALL_THICKNESS]} position={[UNIT * 6, WALL_HEIGHT/2, 0]} rotation={[0, Math.PI/2, 0]} texture={wallTexture} />
+
+      {/* Room 1 (Top Left) */}
+      <Wall size={[UNIT * 4, WALL_HEIGHT, WALL_THICKNESS]} position={[-UNIT * 4, WALL_HEIGHT/2, UNIT * 2]} texture={wallTexture} />
+      <Wall size={[UNIT * 4, WALL_HEIGHT, WALL_THICKNESS]} position={[-UNIT * 0.99, WALL_HEIGHT/2, UNIT * 4]} rotation={[0, Math.PI/2, 0]} texture={wallTexture} />
+
+      {/* Room 2 (Top Right) */}
+      <Wall size={[UNIT * 6, WALL_HEIGHT, WALL_THICKNESS]} position={[UNIT * 3, WALL_HEIGHT/2, UNIT * 2]} texture={wallTexture} />
+
+      {/* Room 3 (Center) */}
+      <Wall size={[UNIT * 5, WALL_HEIGHT, WALL_THICKNESS]} position={[-1, WALL_HEIGHT/2, -6]} texture={wallTexture} />
+
+      {/* Room 4 (Bottom Left) */}
+      <Wall size={[UNIT * 4, WALL_HEIGHT, WALL_THICKNESS]} position={[-UNIT * 3, WALL_HEIGHT/2, -UNIT * 2]} texture={wallTexture} />
+      <Wall size={[UNIT * 4, WALL_HEIGHT, WALL_THICKNESS]} position={[-UNIT * 2, WALL_HEIGHT/2, -UNIT * 4]} rotation={[0, Math.PI/2, 0]} texture={wallTexture} />
+
+      {/* Room 5 (Bottom Right) */}
+      <Wall size={[UNIT * 2, WALL_HEIGHT, WALL_THICKNESS]} position={[UNIT * 5, WALL_HEIGHT/2, -UNIT * 2]} texture={wallTexture} />
+      <Wall size={[UNIT * 4, WALL_HEIGHT, WALL_THICKNESS]} position={[UNIT * 3, WALL_HEIGHT/2, -UNIT * 4]} rotation={[0, Math.PI/2, 0]} texture={wallTexture} />
+    </group>
+  )
+}
+
+// Helper component for walls
+function Wall({ size, position, rotation = [0, 0, 0], texture }) {
+  return (
+    <mesh position={position} rotation={rotation} receiveShadow castShadow>
+      <boxGeometry args={size} />
+      <meshStandardMaterial map={texture} roughness={0.8} metalness={0.2} />
+    </mesh>
+  )
+} 
